29bd6cce7591740c7ad996a487c463a4b9d1e58e
MohammedTabarakAhmed <tabarakahmed030@gmail.com>
2026-08-28 14:43:18 +0530

    a basic blog generator!

---
+++ BlogGeneration/src/nodes/blognode.py
@@ -0,0 +1,71 @@
+from src.state.blog_state import BlogState, Blog
+from langchain_core.messages import SystemMessage, HumanMessage
+
+class BlogNode:
+    """
+    Class to represent the blog node.
+    """
+
+    def __init__(self, llm):
+        self.llm = llm
+
+    def title_generation(self, state: BlogState):
+        """
+        Create the title for the blog.
+        """
+        prompt="""
+                You are an expert blog content writer. Use markdown formatting. Generate 
+                a blog title for the {topic}. This title should be creative and SEO friendly.
+                """
+        system_message=prompt.format(topic=state["topic"])
+        response=self.llm.invoke(system_message)
+        return {"blog":Blog(title=response.content, content="")}
+    
+    def content_generation(self, state: BlogState):
+        """
+        Create the content for the blog.
+        """
+        prompt="""
+                You are an expert blog content writer. Use markdown formatting.
+                Generate a detailed blog content with detailed breakdown for the {topic}.
+                """
+        system_message=prompt.format(topic=state["topic"])
+        response=self.llm.invoke(system_message)
+        return {"blog":Blog(title=state['blog'].title, content=response.content)}
+
+    def translation(self,state:BlogState):
+        """
+        Translate the content to the specified language.
+        """
+        translation_prompt="""
+        Translate the following content into {current_language}.
+        - Maintain the original tone, style, and formatting.
+        - Adapt cultural references and idioms to be appropriate for {current_language}.
+
+        ORIGINAL CONTENT:
+        {blog_content}
+
+        """
+        print(state["current_language"])
+        blog_content=state["blog"]["content"]
+        messages=[
+            HumanMessage(translation_prompt.format(current_language=state["current_language"], blog_content=blog_content))
+
+        ]
+        transaltion_content = self.llm.with_structured_output(Blog).invoke(messages)
+        return {"blog": {"content": transaltion_content}}
+
+    def route(self, state: BlogState):
+        return {"current_language": state['current_language']}
+    
+
+    def route_decision(self, state: BlogState):
+        """
+        Route the content to the respective translation function.
+        """
+        if state["current_language"] == "hindi":
+            return "hindi"
+        elif state["current_language"] == "french": 
+            return "french"
+        else:
+            return state['current_language']
